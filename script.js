4ae2e2ceeea799498beaddb2d8391c18961387d9
divyesh-webdev-dp <webdevdp@yahoo.com>
2023-10-29 11:27:09 +0530

    Feat Slider Two

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 AOS.init();
-
-$(document).on("click","#mobile-menu-btn",function(){
-    if($("#mobile-menu").hasClass("hidden")){
-        $("#mobile-menu").removeClass("hidden");
-        // $("#mobile-menu").attr("data-aos","fade-down");
-    }else{
-        $("#mobile-menu").addClass("hidden"); 
-    }
-});
 $(document).ready(function() {
+    let anchorSelector = 'a[href^="#"]';
+    let anchorList =  document.querySelectorAll(anchorSelector);
+    anchorList.forEach(link => {
+        link.onclick = function (e) {
+            e.preventDefault();
+            let destination = 
+                document.querySelector(this.hash);
+            destination.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
     $("#owl-main").owlCarousel({
         navigation: false, // Show next and prev buttons
         slideSpeed: 300,
@@ -16,13 +19,25 @@ $(document).ready(function() {
         singleItem: true,
         autoHeight : false
     });
+    $("#owl-second").owlCarousel({
+        items: 3,
+        lazyLoad: true,
+        navigation: false,
+    });
+    $(document).on("click","#mobile-menu-btn",function(){
+        if($("#mobile-menu").hasClass("hidden")){
+            $("#mobile-menu").removeClass("hidden");
+            // $("#mobile-menu").attr("data-aos","fade-down");
+        }else{
+            $("#mobile-menu").addClass("hidden"); 
+        }
+    });
+    window.addEventListener('scroll', () => {
+        const verticalScrollPx = window.scrollY;
+        if(verticalScrollPx != 0){
+            $("#home").addClass("bg-black");
+        }else{
+            $("#home").removeClass("bg-black");
+        }
+    });
 });
-window.addEventListener('scroll', () => {
-    const verticalScrollPx = window.scrollY;
-    if(verticalScrollPx != 0){
-        $("#home").addClass("bg-black");
-    }else{
-        $("#home").removeClass("bg-black");
-    }
-  });
-  
